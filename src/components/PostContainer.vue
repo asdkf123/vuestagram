6e9811ab0a51034ef04b11daab30f2dba811713d
lodi <lustean@gmail.com>
2023-04-06 21:31:34 +0900

    vuestagtam 생성

---
+++ src/components/PostContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <PostDocument v-for="(value, i) in instagramData" :key="i"
+    :post="instagramData[i]"
+    />
+  </div>
+</template>
+
+<script>
+import PostDocument from "@/components/PostDocument.vue";
+import instagramData from "@/assets/postData";
+export default {
+  name: "PostContainer",
+  components: {PostDocument},
+  data () {
+    return {
+      instagramData,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PostDocument.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="post" >
+    <div class="post-header">
+<!--      <img :src="post.userImage" class="profile" alt="">-->
+      <div class="profile" :style="{ backgroundImage :`url(${post.userImage}`}"></div>
+      <span class="profile-name">{{post.name}}</span>
+    </div>
+<!--    <img :src="post.postImage" class="post-body">-->
+    <div class="post-body"
+         :style="{ backgroundImage :`url(${post.postImage}`}"></div>
+    <div class="post-content">
+      <p>{{ post.likes }}</p>
+      <p><strong>{{ post.content }}</strong></p>
+      <p class="date">{{ post.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostDocument",
+  props: {
+    post: Object,
+  },
+}
+</script>
+
+<style scoped>
+.post {
+  width: 100%;
+}
+.profile {
+  /*background-image: url("https://placeimg.com/100/100/arch");*/
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  /*background-image: url("https://placeimg.com/640/480/animals");*/
+  height: 450px;
+  width: 100%;
+  /*height: 100%;*/
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
